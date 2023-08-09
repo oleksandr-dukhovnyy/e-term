7698532f4ae24bf037520c7b6d1e7403a44ecb21
Aleksandr Dukhovnyy <script@vip-person.net>
2023-08-09 13:05:01 +0300

    fixed spaces in colored blocks

--- src/js/output-handling/get_colors.js
+++ src/js/output-handling/get_colors.js
@@ -1,27 +1,45 @@
-const getColors = (str) => {
-  // const colors = [
-  //   /* {
-  // 		match: '{color:#f00 | 123 }',
-  // 		color: '#f00',
-  // 		content: '123'
-  // 	} */
-  // ];
+// const getColors = (str) => {
+//   // const colors = [
+//   //   /* {
+//   // 		match: '{color:#f00 | 123 }',
+//   // 		color: '#f00',
+//   // 		content: '123'
+//   // 	} */
+//   // ];
 
-  const mathes = str.match(/\{\s?color:[^|]*\|[^}]*\}/g);
+//   const mathes = str.match(/\{\s?color:[^|]*\|[^}]*\}/g);
 
-  if (!mathes) return [];
+//   if (!mathes) return [];
 
-  return mathes.map((match) => {
-    const rez = {
-      match: match,
+//   return mathes.map((match) => {
+//     const rez = {
+//       match: match,
+//     };
+
+//     const withoutSpaces = match.replace(/\s*/g, '');
+
+//     rez.color = withoutSpaces.replace(/^{color:|\|.*}$/g, '');
+//     rez.content = withoutSpaces.replace(/^.*\||}$/g, '');
+
+//     return rez;
+//   });
+// };
+
+const getColors = (input) => {
+  const matches = input.match(/{\s*color:[^|]+\|[^}]*}/gi);
+
+  if (!matches) return [];
+
+  return matches.map((match) => {
+    const res = /{\s*color:([^|]+)\|([^}]*)}/i.exec(match);
+
+    if (!res) return null;
+
+    return {
+      match,
+      color: res[1].trim(),
+      content: res[2].trim(),
     };
-
-    const withoutSpaces = match.replace(/\s*/g, '');
-
-    rez.color = withoutSpaces.replace(/^{color:|\|.*}$/g, '');
-    rez.content = withoutSpaces.replace(/^.*\||}$/g, '');
-
-    return rez;
   });
 };
 
